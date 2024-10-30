7c4c01ffedf391427a484c49e315c6008e9399e9
Manuel Araujo Baño <manuaraba@gmail.com>
2024-10-30 09:16:18 +0100

    Add synchronous password hashing and comparison using bcrypt

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 
 //END_SYNC
